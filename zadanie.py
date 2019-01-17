24228af1abf7bff57381eafd6787fd5cc7624c03
juliaaw <45161979+juliaaw@users.noreply.github.com>
2019-01-17 11:09:25 +0100

    Update zadanie.py

--- zadanie.py
+++ zadanie.py
@@ -12,7 +12,7 @@ czas=int(len(dane['ch1'])/czestProbkowania)
 t=np.linspace(0, czas, czas*czestProbkowania)
 
 #wykres przed filtracja
-plt.plot(t, dane['ch1'][0:czas*czestProbkowania],  color="pink")
+plt.plot(t, dane['ch1'][0:czas*czestProbkowania],  color="b")
 plt.xlabel("Czas [s]")
 plt.ylabel("Amplituda [mV]")
 plt.show()
@@ -22,7 +22,7 @@ przefiltrowany1=ag.pasmowozaporowy(dane['ch1'], czestProbkowania, 49, 51)
 przefiltrowany2=ag.pasmowoprzepustowy(przefiltrowany1, czestProbkowania, 1, 50)
 
 #wykres po filtracji
-plt.plot(t, przefiltrowany2 [0:czas*czestProbkowania], color="pink")
+plt.plot(t, przefiltrowany2 [0:czas*czestProbkowania], color="b")
 plt.xlabel("Czas [s]")
 plt.ylabel("Amplituda [mV]")
 plt.show()
